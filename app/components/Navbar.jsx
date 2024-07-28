577844c47124d5d00c4b37943d1e104b58dc32c4
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2024-07-28 14:39:52 +0500

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -23,17 +23,34 @@ const Navbar = () => {
 {/*             <p className='text-md font-medium'>Info</p>
             <p className='text-md font-medium text-center'>Wall of Love<i className='bi bi-heart'></i></p> */}
         </div>
-        <div className='z-40'>
-            <button className='hidden lg:block border p-2 font-medium border-cyan-500'>Join-Us</button>
-            <button className='block lg:hidden border p-1 font-medium border-cyan-500 relative' onClick={toggleMenu} >Menu</button>
-            <AnimatePresence mode='wait'>
-              { menu && <motion.div initial={{translateY:-100,opacity:0}} animate={{translateY:0,opacity:1}} exit={{translateY:-100,opacity:0}} transition={{duration:0.3,ease:'linear'}} className='z-0 absolute top-12 left-0 block text-lg text-center bg-cyan-500 rounded-2xl w-full '>
-                  <p><a href={gmailUrl} target='_blank'  rel="noopener noreferrer">Join Waitlist</a></p>
-                  <p>Team</p>
-                  {/* <i className='bi bi-heart'></i> */}
-              </motion.div>}
-            </AnimatePresence>
-        </div>
+      <div className='relative z-40'>
+  <button className='hidden lg:block border p-3 font-medium border-cyan-500 bg-indigo-950 text-white rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-1'>
+    Join-Us
+  </button>
+  <button className='block lg:hidden border p-2 font-medium border-cyan-500 bg-indigo-950 text-white rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-1 relative' onClick={toggleMenu}>
+    Menu
+  </button>
+  <AnimatePresence mode='wait'>
+    {menu && (
+      <motion.div
+        initial={{ translateY: -100, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1 }}
+        exit={{ translateY: -100, opacity: 0 }}
+        transition={{ duration: 0.3, ease: 'linear' }}
+        className='z-0 absolute top-12 left-0 text-lg text-center bg-cyan-500 rounded-2xl w-full shadow-lg p-4'
+      >
+        <p>
+          <a href={gmailUrl} target='_blank' rel='noopener noreferrer' className='text-white hover:underline'>
+            Join Waitlist
+          </a>
+        </p>
+        <p className='text-white'>Team</p>
+      </motion.div>
+    )}
+  </AnimatePresence>
+</div>
+
+      
     </div>
   </div> 
   )
