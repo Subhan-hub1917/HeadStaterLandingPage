e59aa3bbf2bc98de4913f495fc318c343c4a9df2
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2024-07-28 14:17:08 +0500

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -17,18 +17,18 @@ const Navbar = () => {
     <div className='z-40 flex relative items-center justify-between bg-transparent text-white'>
         <div className='flex'>
             <img src="" />
-            <h1 className='lg:text-3xl text-xl font-bold'>Headstarter</h1>
+            <h1 className='lg:text-3xl text-xl font-bold'>RUSH Solutions</h1>
         </div>
         <div className='hidden lg:flex space-x-5'>
-            <p className='text-md font-medium'>Info</p>
-            <p className='text-md font-medium text-center'>Wall of Love<i className='bi bi-heart'></i></p>
+{/*             <p className='text-md font-medium'>Info</p>
+            <p className='text-md font-medium text-center'>Wall of Love<i className='bi bi-heart'></i></p> */}
         </div>
         <div className='z-40'>
             <button className='hidden lg:block border p-2 font-medium border-cyan-500'>Join-Us</button>
             <button className='block lg:hidden border p-1 font-medium border-cyan-500 relative' onClick={toggleMenu} >Menu</button>
             <AnimatePresence mode='wait'>
               { menu && <motion.div initial={{translateY:-100,opacity:0}} animate={{translateY:0,opacity:1}} exit={{translateY:-100,opacity:0}} transition={{duration:0.3,ease:'linear'}} className='z-0 absolute top-12 left-0 block text-lg text-center bg-cyan-500 rounded-2xl w-full '>
-                  <p><a href={gmailUrl} target='_blank'  rel="noopener noreferrer">Join-Us</a></p>
+                  <p><a href={gmailUrl} target='_blank'  rel="noopener noreferrer">Join Waitlist</a></p>
                   <p>Team</p>
                   {/* <i className='bi bi-heart'></i> */}
               </motion.div>}
