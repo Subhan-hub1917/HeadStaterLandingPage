19cd83b08fb973a3ebc0e790069e6fc3217a315f
Subhan Qamar <subiq1917@gmail.com>
2024-07-28 09:13:31 +0500

    Google Analytics Added

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-black">
       <head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-9Z50V1ZSBR"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-9Z50V1ZSBR');
+          </script>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
